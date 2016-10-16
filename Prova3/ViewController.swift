00b4d857546fa95f42166ba67dd57c0338bc2f3f
Eudald Bruc <eudaldbruc@gmail.com>
2016-10-16 14:01:02 +0200

    Initial Commit

---
+++ Prova3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prova3
+//
+//  Created by Miguel Angel Muro Cajal on 16/10/16.
+//  Copyright © 2016 Miguel Angel Muro Cajal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
